9d3c001b618130f349e374245ea443b6e9d42307
Kyle Henderson <kthenderson83@gmail.com>
2019-05-17 15:13:21 -0500

    add button gets values now

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
 console.log('JS');
 
+let employees = [];
+
 $(document).ready(domReady);
 
 function domReady() {
     console.log('JQ');
+    $('#addEmployee').on('click', addEmployee);
+}
+
+
+function addEmployee() {
+    let firstName = $('#firstName').val()
+    let lastName = $('#lastName').val()
+    let employeeId = $('#employeeId').val()
+    let title = $('#title').val()
+    let annualSalary = $('#annualSalary').val()
+
+    let newEmployee = {
+        firstName: firstName,
+        lastName: lastName,
+        employeeId: employeeId,
+        title: title,
+        annualSalary: annualSalary
+    }
+    console.log(newEmployee);
+    
 }
