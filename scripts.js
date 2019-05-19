2f1c664db60da5c6123b884f5e198170ffe334c9
Kyle Henderson <kthenderson83@gmail.com>
2019-05-19 12:21:19 -0500

    auto formatting

--- scripts.js
+++ scripts.js
@@ -1,26 +1,33 @@
 console.log('JS');
 
-let employees = [
+let avengers = [
     {
         firstName: 'Steve',
         lastName: 'Rodgers',
-        employeeId: '1',
+        avengerId: '1',
         title: 'Captain America',
         annualSalary: 25000
     },
     {
         firstName: 'Tony',
         lastName: 'Stark',
-        employeeId: '3',
+        avengerId: '3',
         title: 'Iron Man',
-        annualSalary: 550000
+        annualSalary: 30000
     },
     {
         firstName: 'Carol',
         lastName: 'Danvers',
-        employeeId: '2',
+        avengerId: '2',
         title: 'Captain Marvel',
         annualSalary: 25000
+    },
+    {
+        firstName: 'Thor',
+        lastName: 'Odinson',
+        avengerId: '4',
+        title: 'God of Thunder',
+        annualSalary: 30000
     }
 ];
 
@@ -28,13 +35,13 @@ $(document).ready(domReady);
 
 function domReady() {
     console.log('JQ');
-    updateEmployeeTable();
-    $('#addEmployee').on('click', addEmployee);
+    updateAvengerTable();
+    $('#addAvenger').on('click', addAvenger);
     $('input').on('keypress', checkForEnter);
-    $('#employeeTableBody').on('click', '.deleteEmployee button', deleteEmployee);
+    $('#avengerTableBody').on('click', '.delete-avenger button', deleteAvenger);
 } // end of domReady
 
-function addEmployee() {
+function addAvenger() {
     const requiredInputs = $('.required'); // get all required input elements
     $('#errorMsg').html(''); // remove error msg
     $(requiredInputs).removeClass('error-border'); // remove all error borders
@@ -42,23 +49,23 @@ function addEmployee() {
     // get the data from the inputs
     const first = $('#firstName').val();
     const last = $('#lastName').val();
-    const id = $('#employeeId').val();
+    const id = $('#avengerId').val();
     const title = $('#title').val();
     const salary = $('#annualSalary').val();
 
     if (first.length && last.length && id.length && title.length && salary.length) {
         // if all have values, make sure id is available
-        const availableId = employees.filter( x => x.employeeId === id);
+        const availableId = avengers.filter(x => x.avengerId === id);
         if (availableId.length === 0) {
-            let newEmployee = createEmployee(first, last, id, title, Number(salary));
-            employees.push(newEmployee);
+            let newAvenger = createAvenger(first, last, id, title, Number(salary));
+            avengers.push(newAvenger);
             $('input').val('') // clear the inputs
-            updateEmployeeTable(); // update the table
+            updateAvengerTable(); // update the table
             $('#firstName').focus(); // move cursor back to first input
         }
         else {
             $('#errorMsg').html('*ID is already in use, please use another.');
-            $('#employeeId').addClass('error-border');
+            $('#avengerId').addClass('error-border');
         }
     } // end if all inputs have values
     else {
@@ -72,70 +79,69 @@ function addEmployee() {
         $('#errorMsg').html("*Please fill out required fields."); // add error message if an input is missing
     }
 
-} // end of addEmployee
+} // end of addAvenger
 
 function checkForEnter() {
     if (event.which === 13) {
-        addEmployee();
-    } // end call addEmployee if keypress is 'enter'
+        addAvenger();
+    } // end call addAvenger if keypress is 'enter'
 } // end checkForEnter
 
-function updateEmployeeTable() {
-    $('#employeeTableBody').empty();
-    for (let employee of employees) {
-        $('#employeeTableBody').prepend(`
+function updateAvengerTable() {
+    $('#avengerTableBody').empty();
+    for (let avenger of avengers) {
+        $('#avengerTableBody').append(`
             <tr>
-                <td>${employee.firstName}</td>
-                <td>${employee.lastName}</td>
-                <td>${employee.employeeId}</td>
-                <td>${employee.title}</td>
-                <td>${convertToCurrency.format(employee.annualSalary)}</td>
-                <td class="deleteEmployee"><button id="${employee.employeeId}">Delete</button></td>
+                <td>${avenger.firstName}</td>
+                <td>${avenger.lastName}</td>
+                <td>${avenger.avengerId}</td>
+                <td>${avenger.title}</td>
+                <td>${convertToCurrency.format(avenger.annualSalary)}</td>
+                <td class="delete-avenger"><button id="${avenger.avengerId}">Delete</button></td>
             </tr>
         `)
     }// end append row/data to table
     updateTotalMonthly();
-} // end updateEmployeeTable
+} // end updateAvengerTable
 
 function updateTotalMonthly() {
     let totalSalary = 0;
-    for (let employee of employees) {
-        totalSalary += employee.annualSalary;
+    for (let avenger of avengers) {
+        totalSalary += avenger.annualSalary;
     }
     totalMonthly = totalSalary / 12;
     if (totalMonthly > 20000) {
         $('.totalDisplayWrapper h2').addClass('over-budget'); // create red background for monthly salaries
-        $('input, #addEmployee').attr({disabled: true}).addClass('disabled'); //disable inputs and add disable class
+        $('input, #addAvenger').attr({ disabled: true }).addClass('disabled'); //disable inputs and add disable class
         $('#errorMsg').html('*Monthly salary expense is too high. Someone has to be let go.')
     } else {
         $('.totalDisplayWrapper h2').removeClass('over-budget'); // remove red background for monthly salaries
-        $('input, #addEmployee').attr({disabled: false}).removeClass('disabled'); //enable inputs and remove disabled class
+        $('input, #addAvenger').attr({ disabled: false }).removeClass('disabled'); //enable inputs and remove disabled class
     }
     // format totalMonthly to currency. 
     $('#totalMonthly').html(convertToCurrency.format(totalMonthly));
 
 }// end updateTotalMonthly
 
-function deleteEmployee() {
-    for (let i = 0; i < employees.length; i++) {
-        if (employees[i].employeeId === this.id) {
-            employees.splice(i, 1);
+function deleteAvenger() {
+    for (let i = 0; i < avengers.length; i++) {
+        if (avengers[i].avengerId === this.id) {
+            avengers.splice(i, 1);
         }
     }
-    // employees = employees.filter( employee => employee.employeeId != this.id );
-    updateEmployeeTable();
-}// end deleteEmployee
+    updateAvengerTable();
+}// end deleteAvenger
 
-function createEmployee(first, last, id, title, salary) {
-    let newEmployee = {
+function createAvenger(first, last, id, title, salary) {
+    let newAvenger = {
         firstName: first,
         lastName: last,
-        employeeId: id,
+        avengerId: id,
         title: title,
         annualSalary: salary
     }
-    return newEmployee;
-}//createEmployee
+    return newAvenger;
+}//createAvenger
 
 // format numbers to currency
 const convertToCurrency = new Intl.NumberFormat('en-US', {
